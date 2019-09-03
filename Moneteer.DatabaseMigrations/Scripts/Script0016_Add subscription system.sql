37e6d5c612d302648d9817ac5a691922d0cada34
Chris Oliver <cado82@gmail.com>
2019-09-03 10:02:39 +0100

    Add subscription system columns

---
+++ Moneteer.DatabaseMigrations/Scripts/Script0016_Add subscription system.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE identity.users 
+    ADD COLUMN stripe_id varchar NULL,
+    ADD COLUMN subscription_expiry timestamp without time zone NULL,
+    ADD COLUMN trial_expiry timestamp without time zone NOT NULL default CURRENT_TIMESTAMP;
