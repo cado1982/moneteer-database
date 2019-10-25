586c2d6d8de7fdb69983b0245092b712381d8b60
Chris Oliver <cado82@gmail.com>
2019-10-25 19:49:03 +0100

    Add account target for transactions

--- Moneteer.DatabaseMigrations/Scripts/Script0025_Add account target for transaction assignment.sql
+++
@@ -1,3 +0,0 @@
-﻿ALTER TABLE app.transaction_assignment
-	ADD COLUMN account_id uuid NULL references app.account(id) ON DELETE RESTRICT,
-	ADD UNIQUE (transaction_id, account_id);
---
+++ Moneteer.DatabaseMigrations/Scripts/Script0025_Add account target for transaction.sql
@@ -0,0 +1,4 @@
+﻿ALTER TABLE app.transaction
+	ADD COLUMN target_account_id uuid NULL references app.account(id) ON DELETE RESTRICT,
+	ADD CONSTRAINT check__transaction__target_account_id CHECK ((target_account_id IS NULL AND payee_id IS NULL) OR (target_account_id IS NOT NULL AND payee_id IS NULL) OR (target_account_id IS NULL AND payee_id IS NOT NULL)),
+	ADD CONSTRAINT check__transaction__target_account_id_not_empty CHECK (target_account_id <> '00000000-0000-0000-0000-000000000000');
